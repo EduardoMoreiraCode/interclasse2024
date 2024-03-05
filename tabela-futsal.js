458e9b3bc37a6bd022f9b3dbd5760f172f0bb3be
EduardoMoreiraCode <eduu.eduardo09@gmail.com>
2024-03-04 23:34:45 -0300

    fix: notification push futsal

--- tabela-futsal.js
+++ tabela-futsal.js
@@ -4,8 +4,6 @@
 // pontos automáticos
 // saldo de gols automático
 
-
-
 const times = [
     {
         cor: "",
@@ -241,8 +239,6 @@ for (const time of timesOrd) {
         }
     }
 }
-
-alert("Nenhum jogo ocorrendo no momento!");
 
 // PROJETO FASE DE GRUPOS 2024
 // AS TABELAS SERÃO FEITAS COM OBJETOS EM JS
